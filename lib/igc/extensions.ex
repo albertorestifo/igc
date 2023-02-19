207cfd55c97a1f1d19e08b6f53e671edf6fa31ad
Alberto Restifo <alberto@restifo.dev>
2023-02-19 20:56:45 +0100

    Integrate extension parsing

--- lib/igc/extensions.ex
+++ lib/igc/extensions.ex
@@ -1,9 +1,10 @@
 defmodule IGC.Extensions do
   defstruct extensions: []
 
-  @type extensions_list :: [{String.t(), integer(), integer()}]
+  @type definitions :: [{String.t(), integer(), integer()}]
+  @type values :: %{String.t() => String.t()}
   @type t :: %__MODULE__{
-          extensions: extensions_list()
+          extensions: definitions()
         }
 
   @spec parse_line(String.t()) :: {:ok, t()} | {:error, atom}
@@ -14,8 +15,8 @@ defmodule IGC.Extensions do
     end
   end
 
-  @spec read_all_extensions(String.t(), integer(), extensions_list()) ::
-          {:ok, extensions_list()} | {:error, atom}
+  @spec read_all_extensions(String.t(), integer(), definitions()) ::
+          {:ok, definitions()} | {:error, atom}
   defp read_all_extensions(line, left, extensions \\ [])
   defp read_all_extensions(_line, left, extensions) when left == 0, do: {:ok, extensions}
 
@@ -28,13 +29,28 @@ defmodule IGC.Extensions do
     end
   end
 
-  @spec read_extension(String.t(), extensions_list()) ::
-          {:ok, extensions_list(), String.t()} | {:error, atom}
+  @spec read_extension(String.t(), definitions()) ::
+          {:ok, definitions(), String.t()} | {:error, atom}
   defp read_extension(line, extensions) do
     with {:ok, start_byte, rest} <- IGC.Parser.take(line, 2, :int),
          {:ok, end_byte, rest} <- IGC.Parser.take(rest, 2, :int),
          {:ok, short_code, rest} <- IGC.Parser.take(rest, 3) do
       {:ok, extensions ++ [{short_code, start_byte, end_byte}], rest}
+    end
+  end
+
+  @spec parse_values(String.t(), definitions(), values()) :: {:ok, values()} | {:error, atom}
+  def parse_values(line, extensions_def, extensions_values \\ %{})
+  def parse_values(_line, [], extensions_values), do: {:ok, extensions_values}
+
+  def parse_values("", extensions_def, _extensions_values) when length(extensions_def) > 0 do
+    {:error, :missing_extension_value}
+  end
+
+  def parse_values(line, extensions_def, extensions_values) do
+    with [{short_code, start_byte, end_byte} | next_extensions] <- extensions_def,
+         {:ok, value, rest} <- IGC.Parser.take(line, end_byte - start_byte + 1) do
+      parse_values(rest, next_extensions, Map.put(extensions_values, short_code, value))
     end
   end
 end
--- lib/igc/fix.ex
+++ lib/igc/fix.ex
@@ -7,13 +7,13 @@ defmodule IGC.Fix do
           valid?: boolean,
           pressure_altitude: integer,
           gnss_altitude: integer,
-          extensions: extensions()
+          extensions: IGC.Extensions.values()
         }
 
   @type extensions :: %{String.t() => String.t()}
 
   @spec parse_line(String.t(), IGC.Headers.t()) :: {:ok, t()} | {:error, atom}
-  def parse_line(line, _headers) do
+  def parse_line(line, headers) do
     with {:ok, hours, rest} <- IGC.Parser.take(line, 2, :int),
          {:ok, minutes, rest} <- IGC.Parser.take(rest, 2, :int),
          {:ok, seconds, rest} <- IGC.Parser.take(rest, 2, :int),
@@ -21,15 +21,17 @@ defmodule IGC.Fix do
          {:ok, valid?, rest} <- IGC.Parser.take(rest, 1),
          {:ok, pressure_altitude, rest} <- IGC.Parser.take(rest, 5, :int),
          {:ok, gnss_altitude, _rest} <- IGC.Parser.take(rest, 5, :int),
-         {:ok, time} <- Time.new(hours, minutes, seconds) do
+         {:ok, time} <- Time.new(hours, minutes, seconds),
+         {:ok, extensions} <-
+           IGC.Extensions.parse_values(rest, headers.fix_extensions.extensions) do
       {:ok,
        %__MODULE__{
          time: time,
          position: position,
          valid?: valid? == "A",
          gnss_altitude: gnss_altitude,
-         pressure_altitude: pressure_altitude
-         ## TODO: Parse extensions
+         pressure_altitude: pressure_altitude,
+         extensions: extensions
          ## TODO: Parse well-known data
        }}
     end
