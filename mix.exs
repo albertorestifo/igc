7318c41e01811707b14fa47515b346775c99613a
Alberto Restifo <alberto@restifo.dev>
2023-03-06 23:04:54 +0100

    Fix mix config

--- mix.exs
+++ mix.exs
@@ -23,12 +23,14 @@ defmodule IGC.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:snapshy, "~> 0.2"}
+      {:snapshy, "~> 0.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   def package do
     [
+      name: "igc_parser",
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/albertorestifo/igc"}
     ]
