f3a4df68c5c1f99c4fab6d5000b4db1d5e888994
Alberto Restifo <alberto@restifo.dev>
2023-03-11 11:46:35 +0100

    Add export of functions

--- lib/igc.ex
+++ lib/igc.ex
@@ -4,15 +4,27 @@ defmodule IGC do
   """
 
   @doc """
-  Hello world.
+  Parses an IGC file and creates a stream of the following IGC record types:
 
-  ## Examples
+    * `:fix` [B record]: Positional updates
+    * `:pilot_event` [E record]: Pilot event
+    * `:data` [K record]: Additional data as described in the headers
+    * `:satellite_constellation` [F record]: Satellite constellation data and changes
+    * `:logbook` [L records]: Logbook entries
 
-      iex> IGC.hello()
-      :world
-
+  Each stem entry is a tuple following the format `{type, data, headers}`.
   """
-  def hello do
-    :world
+  def parse(stream) do
+    IGC.Parser.parse(stream)
+  end
+
+  @doc """
+  Like `parse/1`, but filters the stream to only the specified types.
+  For example, to only get the positional updates:
+
+      IGC.parse(stream, [:fix])
+  """
+  def parse(stream, filter) do
+    IGC.Parser.parse(stream, filter)
   end
 end
