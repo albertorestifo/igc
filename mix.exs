9a3cf0f6c928365bdce9dadecc7a03e6e35676d7
Alberto Restifo <alberto@restifo.dev>
2023-03-11 15:29:47 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IGC.MixProject do
   def project do
     [
       app: :igc,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: "IGC file parser",
